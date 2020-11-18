adda0b79716159c71f02e587d8cdeb72d73a688a
沈鑫_SHB_资讯室 <C2082@hanbell.com.cn>
2020-11-18 10:19:18 +0800

    2020/11/18 企业微信回调授权http请求

---
+++ Hanbell-WCO/src/java/cn/hanbell/wco/servlet/QyWechatCallBack.java
@@ -0,0 +1,46 @@
+package cn.hanbell.wco.servlet;
+
+import cn.hanbell.wco.ejb.Agent1000002Bean;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author C2082
+ */
+public class QyWechatCallBack extends HttpServlet {
+
+    protected final Logger log4j = LogManager.getLogger("cn.hanbell.wco");
+
+    @EJB
+    private Agent1000002Bean agent1000002Bean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("content-type", "text/html;charset=UTF-8");
+        String code = req.getParameter("code");
+        String state = req.getParameter("state");
+        log4j.info("--------code=" + code + "---------" + "state=" + state);
+        String userid = agent1000002Bean.getUserIdByCode(code);
+        log4j.info("--------userid=" + userid);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+}
