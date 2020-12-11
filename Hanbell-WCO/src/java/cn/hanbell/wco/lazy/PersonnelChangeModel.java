59c3b34eaaa80e3922a5320d5aa5f1b082dd93d3
沈鑫_SHB_资讯室 <C2082@hanbell.com.cn>
2020-12-11 15:17:31 +0800

    2020/12/11 企业微信催签个人异动单

--- Hanbell-WCO/src/java/cn/hanbell/wco/lazy/PersonnelChangeModel.java
+++ Hanbell-WCO/src/java/cn/hanbell/wco/lazy/PersonnelChangeModel.java
@@ -8,6 +8,9 @@ package cn.hanbell.wco.lazy;
 import cn.hanbell.eap.entity.PersonnelChange;
 import com.lightshell.comm.BaseLazyModel;
 import com.lightshell.comm.SuperEJB;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -15,7 +18,35 @@ import com.lightshell.comm.SuperEJB;
  */
 public class PersonnelChangeModel extends BaseLazyModel<PersonnelChange>{
     
+   private  List<PersonnelChange> selectData=new ArrayList<>();
+    
     public PersonnelChangeModel(SuperEJB superEJB) {
         this.superEJB = superEJB;
+    }
+
+    public PersonnelChange getRowData(String rowKey) {
+        if (this.dataList != null) {
+            Iterator var2 = this.dataList.iterator();
+            while (var2.hasNext()) {
+                PersonnelChange PersonnelChange = (PersonnelChange) var2.next();
+                if (rowKey.equals(String.valueOf(PersonnelChange.getId()))) {
+                    selectData.add(PersonnelChange);
+                    return PersonnelChange;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Object getRowKey(PersonnelChange object) {
+        return super.getRowKey(object);
+    }
+
+    public List<PersonnelChange> getSelectData() {
+        return selectData;
+    }
+
+    public void setSelectData(List<PersonnelChange> selectData) {
+        this.selectData = selectData;
     }
 }
